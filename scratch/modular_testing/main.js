f59edf9065e76f1aa4b57fd7925b0f20dc8b8a68
Claire Simpson <claire.simpson@usda.gov>
2022-04-01 14:03:50 -0600

    Add all geojson layers to main

--- scratch/modular_testing/main.js
+++ scratch/modular_testing/main.js
@@ -81,13 +81,109 @@ require([
       }
     };
 
+    // Create all Tongass Layers
+    const tongass_huc_10 = new GeoJSONLayer({
+      url: "https://storage.googleapis.com/lcms-dashboard/LCMS-Summaries-TongassHuc10-outID.geojson",
+      renderer: renderer,
+      title: 'Tongass HUC 10'
+    });
+    const tongass_huc_8 = new GeoJSONLayer({
+      url: "https://storage.googleapis.com/lcms-dashboard/LCMS-Summaries-TongassHuc8-outID.geojson",
+      renderer: renderer,
+      title: 'Tongass HUC 8'
+    });
+    const tongass_huc_6 = new GeoJSONLayer({
+      url: "https://storage.googleapis.com/lcms-dashboard/LCMS-Summaries-TongassHuc6-outID.geojson",
+      renderer: renderer,
+      title: 'Tongass HUC 6'
+    });
+    const tongass_hex_m = new GeoJSONLayer({
+      url: "https://storage.googleapis.com/lcms-dashboard/LCMS-Summaries-Hex_M_Tongass-outID.geojson",
+      renderer: renderer,
+      title: 'Tongass Hexagon (Sm.)'
+    });
+    const tongass_hex_l = new GeoJSONLayer({
+      url: "https://storage.googleapis.com/lcms-dashboard/LCMS-Summaries-Hex_L_Tongass-outID.geojson",
+      renderer: renderer,
+      title: 'Tongass Hexagon (Med.)'
+    });
+    const tongass_hex_xl = new GeoJSONLayer({
+      url: "https://storage.googleapis.com/lcms-dashboard/LCMS-Summaries-Hex_XL_Tongass-outID.geojson",
+      renderer: renderer,
+      title: 'Tongass Hexagon (Lrg.)'
+    });
+    const tongass_ecosection = new GeoJSONLayer({
+      url: "https://storage.googleapis.com/lcms-dashboard/LCMS-Summaries-TongassNationalForestEcologicalSubsections-outID.geojson",
+      renderer: renderer,
+      title: 'Tongass Eco. Subsections'
+    });
+    const tongass_lta = new GeoJSONLayer({
+      url: "https://storage.googleapis.com/lcms-dashboard/LCMS-Summaries-TongassNationalForestLandTypeAssociations-outID.geojson",
+      renderer: renderer,
+      title: 'Tongass LTAs'
+    });
+
+    // Create all Chugach layers here
+    const chugach_huc10 = new GeoJSONLayer({
+      url: "https://storage.googleapis.com/lcms-dashboard/LCMS-Summaries-ChugachHuc10-outID.geojson",
+      renderer: renderer,
+      title: 'Chugach HUC 10'
+    });
+    const chugach_huc8 = new GeoJSONLayer({
+      url: "https://storage.googleapis.com/lcms-dashboard/LCMS-Summaries-ChugachHuc8-outID.geojson",
+      renderer: renderer,
+      title: 'Chugach HUC 8'
+    });
+    const chugach_huc6 = new GeoJSONLayer({
+      url: "https://storage.googleapis.com/lcms-dashboard/LCMS-Summaries-ChugachHuc6-outID.geojson",
+      renderer: renderer,
+      title: 'Chugach HUC 6'
+    });
+    const chugach_natl_forest_boundary = new GeoJSONLayer({
+      url: "https://storage.googleapis.com/lcms-dashboard/LCMS-Summaries-Chugach_National_Forest_Boundary-outID.geojson",
+      renderer: renderer,
+      title: 'Chugach NF Boundary'
+    });
+    const chugach_natl_forest_ecosection = new GeoJSONLayer({
+      url: "https://storage.googleapis.com/lcms-dashboard/LCMS-Summaries-Chugach_National_Forest_Ecosection-outID.geojson",
+      renderer: renderer,
+      title: 'Chugach Eco. Subsections'
+    });
+    const chugach_natl_forest_lta = new GeoJSONLayer({
+      url: "https://storage.googleapis.com/lcms-dashboard/LCMS-Summaries-Chugach_National_Forest_Land_Type_Association-outID.geojson",
+      renderer: renderer,
+      title: 'Chugach LTAs'
+    });
+    const chugach_natl_forest_hex_m = new GeoJSONLayer({
+      url: "https://storage.googleapis.com/lcms-dashboard/LCMS-Summaries-Hex_M_Chugach-outID.geojson",
+      renderer: renderer,
+      title: 'Chugach Hexagaons (Sm.)'
+    });
+    const chugach_natl_forest_hex_l = new GeoJSONLayer({
+      url: "https://storage.googleapis.com/lcms-dashboard/LCMS-Summaries-Hex_L_Chugach-outID.geojson",
+      renderer: renderer,
+      title: 'Chugach Hexagons (Med.)'
+    });
+    const chugach_natl_forest_hex_xl = new GeoJSONLayer({
+      url: "https://storage.googleapis.com/lcms-dashboard/LCMS-Summaries-Hex_XL_Chugach-outID.geojson",
+      renderer: renderer,
+      title: 'Chugach Hexagons (Lrg.)'
+    });
+
+    // First instance of layer
+    let layer = new GeoJSONLayer({
+      url: "https://storage.googleapis.com/lcms-dashboard/LCMS-Summaries-Hex_XL_Chugach-outID.geojson",
+      renderer: renderer,
+      title: 'Chugach Hexagons (Lrg.)'
+    });
+
 
     // LOAD IN LAYERS - look in template for layer list widget. 
 
-    const layer = new GeoJSONLayer({
-      url: "../../geojson/LCMS-Summaries-Chugach_National_Forest_Ecosection-outID.geojson",//"https://storage.googleapis.com/lcms-dashboard/LCMS-Summaries-Chugach_National_Forest_Ecosection-outID.geojson",
-      renderer: renderer
-    });
+    // const layer = new GeoJSONLayer({
+    //   url: "../../geojson/LCMS-Summaries-Chugach_National_Forest_Ecosection-outID.geojson",//"https://storage.googleapis.com/lcms-dashboard/LCMS-Summaries-Chugach_National_Forest_Ecosection-outID.geojson",
+    //   renderer: renderer
+    // });
 
       
     function getArea(polygon) {
