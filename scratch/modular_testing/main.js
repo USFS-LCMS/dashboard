449c4a110ee254d8a19a0c4925dd6a0194640e7e
WYATT C. MCCURDY <Wyatt.Mccurdy@usda.gov>
2022-03-31 23:04:50 -0600

    comment code
    comment bits of code that are already in main branch - this should leave only changes remaining so that we can more easily move the changes over

--- scratch/modular_testing/main.js
+++ scratch/modular_testing/main.js
@@ -1,4 +1,16 @@
 /*
+Note: This below script is a branch with only the layer selection dropdown and the pan/zoom/click functionality of the chart uncommented. 
+
+This will hopefully make it more clear which parts of the script to bring over to the main branch - I think that just merging the two will cause wierd conflicts. We may need to do some strategic copy and paste. 
+
+
+
+
+*/ 
+
+
+
+/*
 Authors: 
 - Wyatt McCurdy
 - Claire Simpson
@@ -304,166 +316,175 @@ require([
           }
          });
        });
-      // ONCLICK AND USER SCROLL FUNCTIONALITY FOR SIDE CHART UPDATES. ABOVE ^
-
-          /////////////////////////////////////////////////////////////////////////////////////////////////
-    //////// CODE TO DRAW RECTANGLE TO SELECT MULTIPLE POLYGON FEATURES 
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-
-    function getArea(polygon) {
-      // const geodesicArea = geometryEngine.geodesicArea(polygon, "square-kilometers");
-      const planarArea = geometryEngine.planarArea(polygon, "square-kilometers");
-      // console.log(geodesicArea+"geodes area");
-      // console.log(planarArea+"planar area")
-      return planarArea;
-
-    }
-
-    let myLayerView;
-    layer
-      .when(() => {
-        view.whenLayerView(layer).then(function (layerView) {
-          myLayerView = layerView;
-        });
-      });
-      // .catch(errorCallback);
-
-    // create a new instance of a FeatureTable
-    const featureTable = new FeatureTable({
-      view: view,
-      layer: layer,
-      highlightOnRowSelectEnabled: true,
-      fieldConfigs: [
-        {
-          name: "outID",
-          label: "outID"
-        },       
-      
-      ],
-      container: document.getElementById("tableDiv")
-    });
-
-    // this array will keep track of selected feature objectIds to
-    // sync the layerview feature effects and feature table selection
-    let features = [];
-
-    // Listen for the table's selection-change event
-    featureTable.on("selection-change", (changes) => {
-      // if the feature is unselected then remove the objectId
-      // of the removed feature from the features array
-      changes.removed.forEach((item) => {
-        const data = features.find((data) => {
-          return data === item.outID;
-        });
-        if (data) {
-          features.splice(features.indexOf(data), 1);
-        }
-      });
-
-      // If the selection is added, push all added selections to array
-      changes.added.forEach((item) => {
-        features.push(item.outID);
-      });
-
-      // set excluded effect on the features that are not selected in the table
-      myLayerView.featureEffect = {
-        filter: {
-          objectIds: features
-        },
-        excludedEffect: "grayscale(50%)"//"blur(5px) grayscale(90%) opacity(40%)"
-      };
-    });
-
-    // polygonGraphicsLayer will be used by the sketchviewmodel
-    // show the polygon being drawn on the view
-    const polygonGraphicsLayer = new GraphicsLayer();
-    map.add(polygonGraphicsLayer);
-
-    // add the select by rectangle button the view
-    const selectButton = document.getElementById("select-by-rectangle");
-
-    // click event for the select by rectangle button
-    selectButton.addEventListener("click", () => {
-      view.popup.close();
-      sketchViewModel.create("rectangle");
-    });
-
-    // add the clear selection button the view
-    document
-      .getElementById("clear-selection")
-      .addEventListener("click", () => {
-        featureTable.clearSelection();
-        featureTable.filterGeometry = null;
-        polygonGraphicsLayer.removeAll();
-      });
-
-    // create a new sketch view model set its layer
-    const sketchViewModel = new SketchViewModel({
-      view: view,
-      layer: polygonGraphicsLayer
-    });
-
-    // Once user is done drawing a rectangle on the map
-    // use the rectangle to select features on the map and table
-    sketchViewModel.on("create", async (event) => {
-      if (event.state === "complete") {
-        // this polygon will be used to query features that intersect it
-        const geometries = polygonGraphicsLayer.graphics.map(function (
-          graphic
-        ) {
-          return graphic.geometry;
-        });
-        const queryGeometry = await geometryEngineAsync.union(
-          geometries.toArray()
-        );
-        selectFeatures(queryGeometry);
-      }
-    });
-
-    // This function is called when user completes drawing a rectangle
-    // on the map. Use the rectangle to select features in the layer and table
-    function selectFeatures(geometry) {
-      if (myLayerView) {
-        // create a query and set its geometry parameter to the
-        // rectangle that was drawn on the view
-        const query = {
-          geometry: geometry,
-          outFields: ["*"]
-        };
-
-        // query graphics from the csv layer view. Geometry set for the query
-        // can be polygon for point features and only intersecting geometries are returned
-        myLayerView
-          .queryFeatures(query)
-          .then((results) => {
-            if (results.features.length === 0) {
-              clearSelection();
-            } else {
+    // ONCLICK AND USER SCROLL FUNCTIONALITY FOR SIDE CHART UPDATES. ABOVE ^
 
 
-              // here we get to use queried features. chart here
-              // ['Change','Land_Cover','Land_Use'].map((w) => empty_chart.setContentInfo(results,w,"side-chart"));
+    // BELOW CODE COMMENTED OUT - JUST DID THIS IN ORDER TO BRING FOCUS TO THIS BRANCH'S CHANGES - CODE SHOULD STILL WORK TOGETHER ^
 
-              // pass in the query results to the table by calling its selectRows method.
-              // This will trigger FeatureTable's selection-change event
-              // where we will be setting the feature effect on the csv layer view
-              featureTable.filterGeometry = geometry;
-              featureTable.selectRows(results.features);
-            }
-          });
-          // .catch(errorCallback);
-      }
-    }
+        /////////////////////////////////////////////////////////////////////////////////////////////////
+        //////// CODE TO DRAW RECTANGLE TO SELECT MULTIPLE POLYGON FEATURES 
+        ////////////////////////////////////////////////////////////////////////////////////////////////
 
-    function errorCallback(error) {
-      console.log("error happened:", error.message);
-    }
+        // function getArea(polygon) {
+        //   // const geodesicArea = geometryEngine.geodesicArea(polygon, "square-kilometers");
+        //   const planarArea = geometryEngine.planarArea(polygon, "square-kilometers");
+        //   // console.log(geodesicArea+"geodes area");
+        //   // console.log(planarArea+"planar area")
+        //   return planarArea;
+
+        // }
+
+        // let myLayerView;
+        // layer
+        //   .when(() => {
+        //     view.whenLayerView(layer).then(function (layerView) {
+        //       myLayerView = layerView;
+        //     });
+        //   });
+        //   // .catch(errorCallback);
+
+        // // create a new instance of a FeatureTable
+        // const featureTable = new FeatureTable({
+        //   view: view,
+        //   layer: layer,
+        //   highlightOnRowSelectEnabled: true,
+        //   fieldConfigs: [
+        //     {
+        //       name: "outID",
+        //       label: "outID"
+        //     },       
+          
+        //   ],
+        //   container: document.getElementById("tableDiv")
+        // });
+
+        // // this array will keep track of selected feature objectIds to
+        // // sync the layerview feature effects and feature table selection
+        // let features = [];
+
+        // // Listen for the table's selection-change event
+        // featureTable.on("selection-change", (changes) => {
+        //   // if the feature is unselected then remove the objectId
+        //   // of the removed feature from the features array
+        //   changes.removed.forEach((item) => {
+        //     const data = features.find((data) => {
+        //       return data === item.outID;
+        //     });
+        //     if (data) {
+        //       features.splice(features.indexOf(data), 1);
+        //     }
+        //   });
+
+        //   // If the selection is added, push all added selections to array
+        //   changes.added.forEach((item) => {
+        //     features.push(item.outID);
+        //   });
+
+        //   // set excluded effect on the features that are not selected in the table
+        //   myLayerView.featureEffect = {
+        //     filter: {
+        //       objectIds: features
+        //     },
+        //     excludedEffect: "grayscale(50%)"//"blur(5px) grayscale(90%) opacity(40%)"
+        //   };
+        // });
+
+        // // polygonGraphicsLayer will be used by the sketchviewmodel
+        // // show the polygon being drawn on the view
+        // const polygonGraphicsLayer = new GraphicsLayer();
+        // map.add(polygonGraphicsLayer);
+
+        // // add the select by rectangle button the view
+        // const selectButton = document.getElementById("select-by-rectangle");
+
+        // // click event for the select by rectangle button
+        // selectButton.addEventListener("click", () => {
+        //   view.popup.close();
+        //   sketchViewModel.create("rectangle");
+        // });
+
+        // // add the clear selection button the view
+        // document
+        //   .getElementById("clear-selection")
+        //   .addEventListener("click", () => {
+        //     featureTable.clearSelection();
+        //     featureTable.filterGeometry = null;
+        //     polygonGraphicsLayer.removeAll();
+        //   });
+
+        // // create a new sketch view model set its layer
+        // const sketchViewModel = new SketchViewModel({
+        //   view: view,
+        //   layer: polygonGraphicsLayer
+        // });
+
+        // // Once user is done drawing a rectangle on the map
+        // // use the rectangle to select features on the map and table
+        // sketchViewModel.on("create", async (event) => {
+        //   if (event.state === "complete") {
+        //     // this polygon will be used to query features that intersect it
+        //     const geometries = polygonGraphicsLayer.graphics.map(function (
+        //       graphic
+        //     ) {
+        //       return graphic.geometry;
+        //     });
+        //     const queryGeometry = await geometryEngineAsync.union(
+        //       geometries.toArray()
+        //     );
+        //     selectFeatures(queryGeometry);
+        //   }
+        // });
+
+        // // This function is called when user completes drawing a rectangle
+        // // on the map. Use the rectangle to select features in the layer and table
+        // function selectFeatures(geometry) {
+        //   if (myLayerView) {
+        //     // create a query and set its geometry parameter to the
+        //     // rectangle that was drawn on the view
+        //     const query = {
+        //       geometry: geometry,
+        //       outFields: ["*"]
+        //     };
+
+        //     // query graphics from the csv layer view. Geometry set for the query
+        //     // can be polygon for point features and only intersecting geometries are returned
+        //     myLayerView
+        //       .queryFeatures(query)
+        //       .then((results) => {
+        //         if (results.features.length === 0) {
+        //           clearSelection();
+        //         } else {
 
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-      
-      // PDF Download Class
-      const d_pdf = DownloadPDF({});
+        //           // here we get to use queried features. chart here
+        //           // ['Change','Land_Cover','Land_Use'].map((w) => empty_chart.setContentInfo(results,w,"side-chart"));
+
+        //           // pass in the query results to the table by calling its selectRows method.
+        //           // This will trigger FeatureTable's selection-change event
+        //           // where we will be setting the feature effect on the csv layer view
+        //           featureTable.filterGeometry = geometry;
+        //           featureTable.selectRows(results.features);
+        //         }
+        //       });
+        //       // .catch(errorCallback);
+        //   }
+        // }
+
+
+
+
+
+        // function errorCallback(error) {
+        //   console.log("error happened:", error.message);
+        // }
+
+
+        // ////////////////////////////////////////////////////////////////////////////////////////////////
+          
+        //   // PDF Download Class
+        //   const d_pdf = DownloadPDF({});
+
+        // ABOVE CODE COMMENTED OUT - JUST DID THIS IN ORDER TO BRING FOCUS TO THIS BRANCH'S CHANGES - CODE SHOULD STILL WORK TOGETHER ^
 
 
       // TOGGLE SIDEBAR VISIBILITY ***
@@ -474,9 +495,6 @@ require([
       toggleSidebar.hamburgerToggle("hamburger", "multilevel-accordion-menu"); //out div, inner div
 
       toggleSidebar.hamburgerToggle("accordion-item-2", "accordion-item-2-a");
-
-      // toggleSidebar.hamburgerToggle("accordion-item-3", "accordion-item-3-a");
-      // toggleSidebar.hamburgerToggle("accordion-item-4", "accordion-item-4-a");
 
       toggleSidebar.hamburgerToggle("accordion-item-1", "r10-questions");
       toggleSidebar.hamburgerToggle("climate-qs", "accordion-item-1-a");
@@ -496,56 +514,35 @@ require([
       toggleSidebar.hamburgerToggle("map-layer-selection", "map-layer-selection-items");
 
 
-      // ************************************************************  // testing function below > 
+      // COMMENTED OUT THIS BELOW PDF CREATION FUNCTIONALITY - THROWS AN ERROR IN THIS VERSION - WILL NEED TO INCORPORATE CS'S FUNCTIONALITY FROM MAIN BRANCH >
 
-      const get_clicked_radio = (radio_div_id) => { 
-        // should return id of radio div on click, 
-        // then do some other stuff
-        const radio_button_div = document.getElementById(radio_div_id);
-        
-        radio_button_div.addEventListener('click', () => {
-          console.log("aaaaa");
-        })
-      };
+      // Function used to create a chart. 
+      // document.getElementById("pdf-button").addEventListener("click", () => {
+      //   view.takeScreenshot({ format: 'png', width: 2048, height: 2048 }).then(function (screenshot) {
 
-      // get_clicked_radio('chugach-huc10-radio-select');
+      //     const imageElement = document.getElementsByClassName(
+      //       "js-screenshot-image"
+      //     )[0];
 
+      //     imageElement.src = screenshot.dataUrl;
+      //     imageElement.height = screenshot.data.height;
+      //     imageElement.width = screenshot.data.width;
+      //     d_pdf.downloadPDF(storeResults);
 
+      //   });
 
-      
-      // const ids = $('#map-layer-selection-items > .radio-button-wrapper').map(function() {
-      //   return this.id || null;
-      // }).get();
+      // });
 
-      // console.log(ids);
-
-      // ************************************************************ // testing function above ^
+      // COMMENTED OUT THIS ABOVE PDF CREATION FUNCTIONALITY - THROWS AN ERROR IN THIS VERSION - WILL NEED TO INCORPORATE CS'S FUNCTIONALITY FROM MAIN BRANCH ^
 
 
-      // Create an empty chart as CreateChart object
-      // const empty_chart = CreateChart({});
-      // empty_chart.chartVisibleFeatures(view, layer, ["Change---Fast Loss", "Change---Slow Loss"]);
-
-
-      // Function used to create a chart. WM moved to 
-      document.getElementById("pdf-button").addEventListener("click", () => {
-        view.takeScreenshot({ format: 'png', width: 2048, height: 2048 }).then(function (screenshot) {
-
-          const imageElement = document.getElementsByClassName(
-            "js-screenshot-image"
-          )[0];
-
-          imageElement.src = screenshot.dataUrl;
-          imageElement.height = screenshot.data.height;
-          imageElement.width = screenshot.data.width;
-          d_pdf.downloadPDF(storeResults);
-
-        });
-
-      });
 
       // return layer
-    })
+    });
+
+
+
+    // I REMOVED BELOW ".THEN" PART OF SCRIPT TO GET MY FEATURE CLICK FUNCTIONALITY TO WORK - EITHER WE WILL NEED TO FIX THIS OR SUBSTITUTE IN THE VERSION OF THE SCRIPT FROM THE OTHER BRANCH - ONCLICK THROWS AN ERROR
     
     // .then(function (layer) {
 
@@ -600,23 +597,18 @@ require([
     //                   }
     //                 });
     //               });
-    //             //});
-    //             // Chart class
-    //               // const c = CreateChart({});
-    //               // c.createOutputObj(results, ["Change---Fast Loss"], "side-chart"); // OOF, CHANGE THIS, THIS IS JUST HARDCODED 
-    //               // c.setContentInfo(results, "Land_Cover", "side-chart")  
-    //               // c.createOutputObj(results, [button_relationships[document.getElementById("tree-shrub-question").value]])
     //               storeResults = results;
     //             }
-    //           } //)
-    //         });
+    //           } 
+    //       });
 
-    //       }
-
-          
-    //     });
+    //     }
+    // });
 
 
-      // })
+
+    // I REMOVED ABOVE ".THEN" PART OF SCRIPT TO GET MY FEATURE CLICK FUNCTIONALITY TO WORK - EITHER WE WILL NEED TO FIX THIS OR SUBSTITUTE IN THE VERSION OF THE SCRIPT FROM THE OTHER BRANCH
+
+
   }
 );
