b08d6027a0df48b81762e815c94f60491205ac30
Claire Simpson <claire.simpson@usda.gov>
2022-04-11 17:33:15 -0600

    add model for graph info; add links to info modals

--- scratch/modular_testing/main.js
+++ scratch/modular_testing/main.js
@@ -122,6 +122,32 @@ require([
     modalinfos.createModals();
     modalinfos.createInfoModalDict();
 
+    var modal = document.getElementById("myModal_graph");
+
+    // Get the button that opens the modal for graphs
+    var btn = document.getElementById('span_popup_graph');
+
+    // Get the <span> element that closes the modal
+    // var span = document.getElementsByClassName("close")[0];
+    var span = document.getElementById('close_graph');
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+    modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    }
+
 
     ///////// end of modal
 
