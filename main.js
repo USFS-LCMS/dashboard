2a044a32040d7e68bfbcbb4f3198857cbe275310
Claire Simpson <claire.simpson@usda.gov>
2022-04-28 09:58:56 -0600

    Fix zoom on page load (to full layer extent of Chugach)

--- main.js
+++ main.js
@@ -107,41 +107,6 @@ require([
     // Set first (default) image layer
     let img_layer = img_layer_dict['landcover-button-wrapper'];
 
-
-
-
-    
-    /////////////////// modal
-
-    // var mainModal = new bootstrap.Modal(document.getElementById('myModal_main'),{});
-    // mainModal.toggle()
-
-    // Get the modal
-    // var modal = document.getElementById("myModal");
-
-    // // Get the button that opens the modal
-    // var btn = document.getElementById("span_popup1");
-
-    // // Get the <span> element that closes the modal
-    // var span = document.getElementsByClassName("close")[0];
-
-    // // When the user clicks the button, open the modal 
-    // btn.onclick = function() {
-    //   modal.style.display = "block";
-    // }
-
-    // // When the user clicks on <span> (x), close the modal
-    // span.onclick = function() {
-    //   modal.style.display = "none";
-    // }
-
-    // // When the user clicks anywhere outside of the modal, close it
-    // window.onclick = function(event) {
-    //   if (event.target == modal) {
-    //     modal.style.display = "none";
-    //   }
-    // }
-
     ///////// graphs info modal
     
     var modal = document.getElementById("myModal_graph");
@@ -170,34 +135,7 @@ require([
     }
     }
 
-    //////////////////// main info modal
-    // var modal2 = document.getElementById("myModal_main");
-
-    // // Get the button that opens the modal for graphs
-    // var btn2 = document.getElementById('span_popup_main');
-
-    // // Get the <span> element that closes the modal
-    // // var span = document.getElementsByClassName("close")[0];
-    // var span2 = document.getElementById('close_main');
-
-    // // When the user clicks the button, open the modal 
-    // btn2.onclick = function() {
-    // modal2.style.display = "block";
-    // }
-
-    // // When the user clicks on <span> (x), close the modal
-    // span2.onclick = function() {
-    // modal2.style.display = "none";
-    // }
-
-    // // When the user clicks anywhere outside of the modal, close it
-    // window.onclick = function(event) {
-    // if (event.target == modal) {
-    //     modal2.style.display = "none";
-    // }
-    // }
-
-
+    
     ///////// end of modal
 
 
@@ -439,7 +377,7 @@ require([
       //Object.values(featureDict)[0]
       Object.values(featureDict)[0].when(()=>{
         // console.log('setting extent');
-        view.extent = layer.fullExtent;
+        view.goTo(layer.fullExtent)
       });
 
       // const selectorElement = document.getElementById("features-select"); MAYBE BRING BACK LATER
